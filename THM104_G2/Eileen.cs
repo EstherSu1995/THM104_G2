8c124a44337056de5c7712cf8a6b3cacbae6ac78
Eileen <x96432@gmail.com>
2024-01-08 15:21:44 +0800

    add Eileen.cs

---
+++ THM104_G2/Eileen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THM104_G2
+{
+    public class Eileen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("哈囉~我是依姍~");
+        }
+    }
+}
