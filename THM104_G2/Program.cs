71cddc9192012d95e41294a60628dcf6783e568d
Chloe <cherrylin30@gmail.com>
2024-01-08 17:13:42 +0800

    Revert "branch test"
    This reverts commit 0b2e13ddda9469cd5ae5d066ef813db5ca582739.

--- THM104_G2/Program.cs
+++ THM104_G2/Program.cs
@@ -15,7 +15,7 @@ namespace THM104_G2
             SqlDataReader reader = cmd.ExecuteReader();//指到標題
             while (reader.Read()) ;//Read()嘗試指到第一行，指到第一行就會回true，判斷有沒有下一筆
 
-            Console.WriteLine("Chloe");
+            Console.WriteLine("Esther");
 
 
         }
