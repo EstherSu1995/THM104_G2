16e94a5f057a0e96ff064a71286359b61f559acb
Esther <esther840908@gmail.com>
2024-01-08 15:21:46 +0800

    add new class

---
+++ THM104_G2/Esther.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THM104_G2
+{
+    internal class Esther
+    {
+        public void Foo() {
+            Console.WriteLine("THM104");
+        }
+        
+    }
+}
